45ff0939737a53f7de4449e8a60c2c30b59dc901
Eugene Korzun <e.korzun@gmail.com>
2017-07-12 19:13:21 +0300

    index js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import root from './src/root';
-import branch from './src/branch';
+import root from './dist-modules/root';
+import branch from './dist-modules/branch';
 export {root, branch, branch as default};
